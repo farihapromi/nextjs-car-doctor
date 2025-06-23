4e0490f7c798fc47c60ae2b8f984bf87b379de4a
Fariha Promi <farihapromi2611@gmail.com>
2025-06-23 21:41:02 +0600

    Confgure nextjs.config.js

--- src/app/NavbarItems/ServicesSection.jsx
+++ src/app/NavbarItems/ServicesSection.jsx
@@ -170,8 +170,8 @@ const data=[
     <div className='grid grid-cols-12'>
         {
             data.map((item)=>{
-                return <div className='col-span-4' key={item._id}>
-                    <Image src={item.img} width={314} height={208}/>
+                return <div className=' col-span-12 md:col-span-6 lg:col-span-4' key={item._id}>
+                    <Image src={item.img} width={314} height={208} alt="services"/>
 
                 </div>
             }
