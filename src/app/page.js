e7a10a9cc90dac21b1ebd4a97cb1ec8bc88a0fef
Fariha Promi <farihapromi2611@gmail.com>
2025-06-23 21:15:09 +0600

    add navbar

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div>
-      <h1 className='text-blue-600 bg-blue-400'>This is header</h1>
-      <p className='bg-green-600'>Para</p>
+      <h1>This is header</h1>
     </div>
   );
 }
